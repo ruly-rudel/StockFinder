365c47825ffe6dc7e040115c9701dc7c9fd933b4
ruly-rudel <ruly.official@gmail.com>
2015-11-07 21:33:53 +0900

    add Volume column.

--- StockFinder/Model.cs
+++ StockFinder/Model.cs
@@ -40,5 +40,6 @@ namespace StockFinder.model
         public double High { get; set; }
         public double Low { get; set; }
         public double Close { get; set; }
+        public double Volume { get; set; }
     }
 }
